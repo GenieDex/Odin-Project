0274d4e07b785a23c5463215f21549951bc54aec
Dex <123313818+GenieDex@users.noreply.github.com>
2023-07-14 11:25:10 +0200

    Added Eraser function to etch-a-sketch

--- Foundations/04-Etch_a_Sketch/script.js
+++ Foundations/04-Etch_a_Sketch/script.js
@@ -1,6 +1,7 @@
 let slider = document.getElementById("tileNum");
 let currentColor = document.getElementById("currentColor");
 const drawingArea = document.getElementById("drawingArea");
+let eraser = document.getElementById("eraser");
 
 function updateGrid() {
   let squareSize = parseInt(slider.value);
@@ -10,15 +11,35 @@ function updateGrid() {
   createGrid(squareSize);
 }
 
+
+
 function createGrid(squareSize) {
   for (let i = 0; i < squareSize * squareSize; i++) {
     const div = document.createElement("div");
     div.style.cssText = "border: 1px solid grey;";
     drawingArea.appendChild(div);
-    div.addEventListener("click", function (event) {
-      event.target.style.backgroundColor = currentColor.value;
+    div.addEventListener("click", setColor);
+
+    eraser.addEventListener('change', function() {
+      if (this.checked) {
+        div.removeEventListener("click", setColor);
+        div.addEventListener("click", eraseColor);
+      } else {
+        div.removeEventListener("click", eraseColor);
+        div.addEventListener("click", setColor);
+      }
     });
   }
+}
+
+
+
+function setColor(event) {
+  event.target.style.backgroundColor = currentColor.value;
+}
+
+function eraseColor(event) {
+  event.target.style.backgroundColor = "";
 }
 
 function clearGrid() {
@@ -26,4 +47,4 @@ function clearGrid() {
 }
 
 slider.addEventListener("input", updateGrid);
-document.getElementById("clear").addEventListener("click",updateGrid);
+document.getElementById("clear").addEventListener("click", updateGrid);
